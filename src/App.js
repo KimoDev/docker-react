f3e0b98047167d48f0f3d809793e2c561a6ded50
Kimo <josephmarquanddesigns@gmail.com>
2020-09-21 18:20:17 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React running in Docker
         </a>
       </header>
     </div>
