e91947c8c0c79e4e2852661843e974e331e58b1b
Kimo <josephmarquanddesigns@gmail.com>
2020-09-22 15:04:17 +0100

    Just to mess around with friends again

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://cdn.discordapp.com/emojis/722657884537618472.png?v=1" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://cdn.discordapp.com/emojis/722657884537618472.png?v=1"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          React running in Docker v3!
+          React running in Docker
         </a>
       </header>
     </div>
